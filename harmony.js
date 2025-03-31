0383c4170e145e680da3dd51ac644c48cd952991
zhaowilliam139 <65860194+zhaowilliam139@users.noreply.github.com>
2025-03-31 09:33:20 +0800

    Add files via upload

---
+++ harmony.js
@@ -0,0 +1,37 @@
+var reminder =1;
+
+setInterval(function() {
+  console.log("HarmonyReminder");
+  let ready = false;
+
+  const nodeList = document.querySelectorAll("button[id='environment-selection']");
+
+  if (nodeList != null) {
+    for (let i = 0; i < nodeList.length; i++) {
+      if (nodeList[i].hasAttribute("_ngcontent-ng-c41150646")) {
+        nodeList[i].style.backgroundColor = "red";
+  
+        let btnText = nodeList[i].innerHTML.trim();
+        console.log(btnText);
+
+        if (btnText.startsWith("Available")) {
+          ready = true;
+          reminder = 1;
+          nodeList[i].style.backgroundColor = "blue";
+        } else if (btnText.startsWith("Break")) {
+          ready = true;
+          reminder = 1;
+          nodeList[i].style.backgroundColor = "blue";
+        } else {
+          ready = false;
+        }
+      }
+    }  
+  }
+
+  if (reminder < 10 && ready == false) {
+    reminder++;
+    window.open(chrome.runtime.getURL("notready.wav"), "_blank");
+  }
+
+}, 20000);
