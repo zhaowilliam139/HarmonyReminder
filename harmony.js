52e8c29364925c2b8ba8cddd6f5c7b5310acbbd4
zhaowilliam139 <zhaowilliam@139.com>
2025-03-31 16:53:53 +0800

    增加了电话拨出的处理

--- harmony.js
+++ harmony.js
@@ -9,22 +9,40 @@ setInterval(function() {
   if (nodeList != null) {
     for (let i = 0; i < nodeList.length; i++) {
       if (nodeList[i].hasAttribute("_ngcontent-ng-c41150646")) {
+        //找到了按钮
         nodeList[i].style.backgroundColor = "red";
   
+        //按钮文本
         let btnText = nodeList[i].innerHTML.trim();
         console.log(btnText);
 
         if (btnText.startsWith("Available")) {
+          //状态 Available
           ready = true;
           reminder = 1;
-          nodeList[i].style.backgroundColor = "#5E72E4";
+          nodeList[i].style.backgroundColor = "blue";
         } else if (btnText.startsWith("Break")) {
+          //状态 Break / Lunch
+          let myDate = new Date();
+          console.log("Current time:" + myDate.getHours() + ":" + myDate.getMinutes());
+
+          //午饭时间不提示 12:30~13:30
+          if ((myDate.getHours() >=12 && myDate.getMinutes() >= 30) && (myDate.getHours() <= 13 && myDate.getMinutes() <= 30)) 
+          {
+              ready = true;
+              reminder = 1;
+              nodeList[i].style.backgroundColor = "#5E72E4";
+          }
+        } else if (btnText.startsWith("HANDLING")) {
+          //电话拨入拨出 HANDLING INTERACTION
           ready = true;
           reminder = 1;
           nodeList[i].style.backgroundColor = "#5E72E4";
         } else {
-          ready = false;
+            ready = false;
         }
+      } else {
+        ready = false;
       }
     }  
   }
@@ -34,4 +52,4 @@ setInterval(function() {
     window.open(chrome.runtime.getURL("notready.wav"), "_blank");
   }
 
-}, 60000);
+}, 10000);
