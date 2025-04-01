5e8d33df216ad93af33df963869d8ef64125a51a
zhaowilliam139 <zhaowilliam@139.com>
2025-04-01 12:12:34 +0800

    add Wrap Up

--- harmony.js
+++ harmony.js
@@ -9,40 +9,43 @@ setInterval(function() {
   if (nodeList != null) {
     for (let i = 0; i < nodeList.length; i++) {
       if (nodeList[i].hasAttribute("_ngcontent-ng-c41150646")) {
-        //找到了按钮
+        //Found the button
         nodeList[i].style.backgroundColor = "red";
   
-        //按钮文本
-        let btnText = nodeList[i].innerHTML.trim();
-        console.log(btnText);
+        //Button text
+        let btnText = nodeList[i].innerHTML.trim().toLowerCase();
+        console.log("Harmony button text: " + btnText);
 
-        if (btnText.startsWith("Available")) {
-          //状态 Available
+        if (btnText.startsWith("available")) {
+          //Available
           ready = true;
           reminder = 1;
           nodeList[i].style.backgroundColor = "blue";
-        } else if (btnText.startsWith("Break")) {
-          //状态 Break / Lunch
+        } else if (btnText.startsWith("break")) {
+          //Break / Lunch
           let myDate = new Date();
           console.log("Current time:" + myDate.getHours() + ":" + myDate.getMinutes());
 
-          //午饭时间不提示 12:30~13:30
+          //Lunch 12:30~13:30
           if ((myDate.getHours() >=12 && myDate.getMinutes() >= 30) && (myDate.getHours() <= 13 && myDate.getMinutes() <= 30)) 
           {
               ready = true;
               reminder = 1;
               nodeList[i].style.backgroundColor = "#5E72E4";
           }
-        } else if (btnText.startsWith("HANDLING")) {
-          //电话拨入拨出 HANDLING INTERACTION
+        } else if (btnText.startsWith("handling")) {
+          //Handling Interaction
+          ready = true;
+          reminder = 1;
+          nodeList[i].style.backgroundColor = "#5E72E4";
+        } else if (btnText.startsWith("wrap")) {
+          //Wrap up
           ready = true;
           reminder = 1;
           nodeList[i].style.backgroundColor = "#5E72E4";
         } else {
             ready = false;
         }
-      } else {
-        ready = false;
       }
     }  
   }
@@ -52,4 +55,4 @@ setInterval(function() {
     window.open(chrome.runtime.getURL("notready.wav"), "_blank");
   }
 
-}, 10000);
+}, 60000);
